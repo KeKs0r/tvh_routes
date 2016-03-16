7caa115d3bf1e9f7e0f41399ae333d7978ee6d83
marc <marc.hoeffl@gmail.com>
2016-03-16 01:44:41 +0100

    simul1 boilerplate

--- index.js
+++ index.js
@@ -30,17 +30,21 @@ Async.auto({
             if (err) {
                 return cb(err)
             }
-            var grouped = _.groupBy(res.rows, function (ride) {
+            var grouped = _.groupBy(res, function (ride) {
                 var date = new Moment(ride.ride_date);
                 return [ride.ride_no, ride.driver_emp_id, date.format(dateFormat)].join('_');
             });
-            console.log('Amount of Routes:'+ _.size(grouped) + ' / Points:'+res.rows.length);
+            console.log('Amount of Routes:' + _.size(grouped) + ' / Points:' + res.length);
             cb(null, grouped);
         });
     },
-    insertActual:['getRoutes', function(cb, res){
+    //insertActual: ['getRoutes', function (cb, res) {
+    //    var routes = res.getRoutes;
+    //    Async.eachLimit(routes, 10, insertActual, cb);
+    //}],
+    insertFirstSimulation: ['getRoutes', function (cb, res) {
         var routes = res.getRoutes;
-        Async.eachLimit(routes, 10, insertActual, cb);
+        Async.eachLimit(routes, 10, insertFirstSimulation, cb);
     }]
 }, function (err, res) {
     if (err) {
@@ -55,21 +59,19 @@ Async.auto({
 });
 
 
-
-var insertCounter = 0;
-var insertActual = function(points, cb) {
+var insertActual = function (points, cb) {
     points = _.sortBy(points, 'sequence_no');
     var first = _.first(points);
-    var id = Moment(first.ride_date).format(dateFormat) + ' D:'+first.driver_emp_id+ ' R'+first.ride_no;
+    var id = Moment(first.ride_date).format(dateFormat) + ' D:' + first.driver_emp_id + ' R' + first.ride_no;
     //console.log(id + '  --> '+points.length);
-    Async.times(points.length, function(i, next){
+    Async.times(points.length, function (i, next) {
         //console.log(id + '#'+i);
         var startPoint = points[i];
         var endPoint = points[i + 1] ? points[i + 1] : startPoint; // in case there is only one point
         // the last point cannot be calculated to db
         if (i != points.length - 1 || points.length === 1) {
             getRoute(startPoint, endPoint, function (err, calculatedRouteData) {
-                if(err){
+                if (err) {
                     return cb(err);
                 }
                 var route = {
@@ -85,37 +87,75 @@ var insertActual = function(points, cb) {
             // This causes empty array elements in result
             next(null, null);
         }
-    },function(err,routes){
-        if(err){
+    }, function (err, routes) {
+        if (err) {
             return cb(err);
         }
         // Remove the empty entries from
-        var cleaned = _.filter(routes, function(r) {
+        var cleaned = _.filter(routes, function (r) {
             return r;
         });
         insertRouteDB(cleaned, cb);
     });
 };
 
+var insertFirstSimulation = function (points, cb) {
+    points = _.sortBy(points, 'sequence_no');
+    Async.each(points, function (point, next) {
+        Async.auto({
+            getClosest: function (cb) {
+                findClosestDepot(point, cb)
+            },
+            route: ['getClosest', function (cb, res) {
+                var startPoint = point;
+                var endPoint = res.getClosest;
+                getRoute(startPoint, endPoint, function (err, calculatedRouteData) {
+                    if (err) {
+                        return cb(err);
+                    }
+                    var route = {
+                        route: calculatedRouteData,
+                        startPoint: startPoint,
+                        endPoint: endPoint,
+                        sequenceNumber: i + 1,
+                        rideType: 'Simul 1'
+                    };
+                    cb(null, route);
+                });
+            }]
+        }, function (err, res) {
+            next(err, res.route);
+        });
+    }, function (err, routes) {
+        if (err) {
+            return cb(err);
+        }
+        // Remove the empty entries from
+        var cleaned = _.filter(routes, function (r) {
+            return r;
+        });
+        insertRouteDB(cleaned, cb);
+    });
+};
 
-var insertRouteDB = function(routes, cb){
+var insertRouteDB = function (routes, cb) {
     var q = [
         'INSERT INTO warehouse.rental_transport_routes',
         '   (ride_no, sequence_no, driver_emp_id, ride_date, from_latitude, from_longitude, to_latitude, to_longitude, ride_type, distance, route_time)',
         '   VALUES '
     ];
     var inserts = [];
-    routes.forEach(function(r) {
+    routes.forEach(function (r) {
         var single = '(' + [
                 r.startPoint.ride_no,
                 r.sequenceNumber,
                 r.startPoint.driver_emp_id,
-                "date '"+Moment(r.startPoint.ride_date).format(dateFormat)+"'",
+                "date '" + Moment(r.startPoint.ride_date).format(dateFormat) + "'",
                 r.startPoint.latitude,
                 r.startPoint.longitude,
                 r.endPoint.latitude,
                 r.endPoint.longitude,
-                '\''+r.rideType+'\'',
+                '\'' + r.rideType + '\'',
                 r.route.totalDistance,
                 r.route.totalTime
             ].join(',') + ' ) ';
@@ -126,6 +166,30 @@ var insertRouteDB = function(routes, cb){
     db.query(q.join('\n'), cb);
 };
 
+var findClosestDepot = function (point, cb) {
+    Async.auto({
+        getDepots: findAllDepots,
+        mapDistance: ['findAllDepots', function (cb, res) {
+            var depots = res.getDepots;
+            var withDistance = _.map(depots, function (depot) {
+                depot.distance = geolib.getDistance(point, depot);
+                return depot;
+            });
+            cb(null, withDistance)
+        }]
+    }, function (err, res) {
+        if (err) {
+            return cb(err);
+        }
+        var depots = res.mapDistance;
+        var closest = _.first(_.sortBy(depots, 'distance'));
+        cb(null, closest);
+    });
+};
+
+var findAllDepots = Async.memoize(function (cb) {
+    DB.query('SELECT * from warehous.rental_depots', cb);
+});
 
 
 var getRoute = function (startPoint, endPoint, callback) {
@@ -217,66 +281,8 @@ var getRoute = function (startPoint, endPoint, callback) {
         // TEMP CODE TO REPLACE SOAP API -- DELETE AFTER TESTING
         calculatedRouteData = {
             totalDistance: geolib.getDistance(startPoint, endPoint),
-            totalTime: geolib.getDistance(startPoint, endPoint),
-        }
+            totalTime: geolib.getDistance(startPoint, endPoint)
+        };
         if (callback) callback(null, calculatedRouteData);
     }
 };
-
-
-
-//*********** LEGACY CODE ******************/
-
-
-
-var insertTransportRoute = function (calculatedRouteData, startPoint, endPoint, sequenceNumber, rideType) {
-    db.connect(conString, function (err, client, done) {
-        if (err) throw err;
-        client.query('INSERT INTO warehouse.rental_transport_routes (ride_no, sequence_no, driver_emp_id, ride_date, from_latitude, from_longitude, to_latitude, to_longitude, ride_type, distance, route_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);', [
-            startPoint.ride_no,
-            sequenceNumber,
-            startPoint.driver_emp_id,
-            startPoint.ride_date,
-            startPoint.latitude,
-            startPoint.longitude,
-            endPoint.latitude,
-            endPoint.longitude,
-            rideType,
-            calculatedRouteData.totalDistance,
-            calculatedRouteData.totalTime
-        ], function (err, result) {
-            if (err) throw err;
-            console.log(result);
-            console.log('** Ride Nr.' + startPoint.ride_no + ' -Ride Date- ' + startPoint.ride_date + ' -Driver Id- ' + startPoint.driver_emp_id + '-Time-' + calculatedRouteData.totalTime + 's -Distance- ' + calculatedRouteData.totalDistance + 'm');
-        });
-    });
-};
-
-
-var oldFunc = function () {
-    db.any('SELECT * FROM warehouse.rental_transport_points WHERE sequence_no=1 ORDER BY ride_date ASC LIMIT 100 OFFSET 0;')
-        .then(function (rides) {
-            _.each(rides, function (ride) {
-                db.any('SELECT * FROM warehouse.rental_transport_points WHERE ride_no=$1 and driver_emp_id=$2 and ride_date=$3 ORDER BY sequence_no ASC;', [ride.ride_no, ride.driver_emp_id, ride.ride_date])
-                    .then(function (points) {
-                        for (var i = 0; i < points.length; i++) {
-                            var startPoint = points[i];
-                            var endPoint = points[i + 1] ? points[i + 1] : startPoint; // in case there is only one point
-                            // the last point cannot be calculated to db
-                            if (i != points.length - 1 || points.length === 1) {
-                                getRoute(startPoint, endPoint, function (calculatedRouteData) {
-                                    console.log(calculatedRouteData);
-                                    // insertTransportRoute(calculatedRouteData, startPoint, endPoint, i+1, 'actual');
-                                });
-                            }
-                        }
-                    });
-            });
-        })
-        .catch(function (error) {
-            throw error;
-        })
-        .finally(function () {
-            pgp.end(); // for immediate app exit, closing the connection pool.
-        });
-}
